573aad8dc4c4603cf4323a1a849eb5bbf4f7dded
avniyadav08 <avniy648@gmail.com>
2026-02-04 00:12:26 +0530

    Cloudinary image upload integration completed

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,178 +1,238 @@
-const Product =require("../models/productModel")
-const ErrorHandler=require("../utils/errorHandler")
-const catchAsyncErrors=require('../middleware/catchAsyncErrors')
-const APIFeatures=require('../utils/apiFeatures');
+const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require('../middleware/catchAsyncErrors');
+const APIFeatures = require('../utils/apiFeatures');
+const cloudinary = require('../config/cloudinary');
 
-//create new Product - admin
-exports.createProduct=catchAsyncErrors(async(req,res,next)=>{
-    req.body.user=req.user.id;
+// Create new Product - admin
+exports.createProduct = catchAsyncErrors(async (req, res, next) => {
+    let images = [];
 
-    const product= await Product.create(req.body);
+    if (typeof req.body.images === 'string') {
+        images.push(req.body.images);
+    } else {
+        images = req.body.images;
+    }
+
+    const imagesLinks = []; // Fixed: was imageLinks
+
+    for (let i = 0; i < images.length; i++) {
+        const result = await cloudinary.uploader.upload(images[i], {
+            folder: 'chronous/products'
+        });
+
+        imagesLinks.push({
+            public_id: result.public_id,
+            url: result.secure_url
+        });
+    }
+
+    req.body.images = imagesLinks;
+    req.body.user = req.user.id;
+
+    const product = await Product.create(req.body);
 
     res.status(201).json({
-        success:true,
+        success: true,
         product
     });
 });
 
-//get all products
-exports.getAllProducts=catchAsyncErrors(async(req,res,next)=>{
-    const resultPerPage=8;
-    const productCount=await Product.countDocuments();
+// Get all products
+exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
+    const resultPerPage = 8;
+    const productsCount = await Product.countDocuments(); // Fixed: was productCount
 
-    const apiFeatures=new APIFeatures(Product.find(),req.query)
-    .search()
-    .filter();
+    const apiFeatures = new APIFeatures(Product.find(), req.query)
+        .search()
+        .filter();
 
-    let products=await apiFeatures.query;
-    let filteredProductsCount=products.length;
+    let products = await apiFeatures.query;
+    let filteredProductsCount = products.length;
 
     apiFeatures.pagination(resultPerPage);
-    products=await apiFeatures.query.clone();
+    products = await apiFeatures.query.clone();
 
     res.status(200).json({
-        success:true,
+        success: true,
         products,
-        productCount,
+        productsCount, // Fixed
         resultPerPage,
         filteredProductsCount
     });
 });
 
-//get single product details
-exports.getProductDetails=catchAsyncErrors(async(req,res,next)=>{
-    const product=await Product.findById(req.params.id);
+// Get single product details
+exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
 
-    if(!product){
-        return next (new ErrorHandler('Productnot found',404));
+    if (!product) {
+        return next(new ErrorHandler('Product not found', 404)); // Fixed: space in 'Product not found'
     }
 
     res.status(200).json({
-        success:true,
+        success: true,
         product
     });
 });
 
+// Update product - admin
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
 
-//update product - admin
-exports.updateProduct=catchAsyncErrors(async(req,res,next)=>{
-    let product=await Product.findById(req.params.id);
-
-    if(!product){
-        return next(new ErrorHandler('Product not found',404));
+    if (!product) {
+        return next(new ErrorHandler('Product not found', 404));
     }
 
-    product=await Product.findByIdAndUpadate(req.params.id,req.body,{
-        new:true,
-        runValidators:true,
-        UserFindAndModify:false
+    let images = [];
+
+    if (typeof req.body.images === 'string') { // Fixed: was typeOf (wrong case)
+        images.push(req.body.images);
+    } else {
+        images = req.body.images; // Fixed: was umages
+    }
+
+    if (images !== undefined) {
+        // Delete old images from cloudinary
+        for (let i = 0; i < product.images.length; i++) {
+            await cloudinary.uploader.destroy(product.images[i].public_id);
+        }
+
+        const imagesLinks = [];
+
+        for (let i = 0; i < images.length; i++) {
+            const result = await cloudinary.uploader.upload(images[i], {
+                folder: 'chronous/products'
+            });
+
+            imagesLinks.push({
+                public_id: result.public_id,
+                url: result.secure_url
+            });
+        }
+
+        req.body.images = imagesLinks;
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, { // Fixed: was findByIdAndUpadate
+        new: true,
+        runValidators: true,
+        useFindAndModify: false // Fixed: was UserFindAndModify
     });
-    
+
     res.status(200).json({
-        success:true,
+        success: true,
         product
     });
 });
 
+// Delete product - admin
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
+    const product = await Product.findById(req.params.id); // Fixed: was req.parmas.id
 
-//delete product -admin
-exports.deleteProduct=catchAsyncErrors(async(req,res,next)=>{
-    const product=await Product.findById(req.parmas.id);
+    if (!product) {
+        return next(new ErrorHandler('Product not found', 404));
+    }
 
-    if(!product){
-        return next(new ErrorHandler('Product not found',404));
+    // Delete images from cloudinary
+    for (let i = 0; i < product.images.length; i++) { // Fixed: was i > product.images.length
+        await cloudinary.uploader.destroy(product.images[i].public_id); // Fixed: was cloudinaru
     }
 
     await product.deleteOne();
 
     res.status(200).json({
-        success:true,
-        message:'Product deleted successfully'
+        success: true,
+        message: 'Product deleted successfully'
     });
 });
 
-//create or update review
-exports.createProductReview=catchAsyncErrors(async(req,res,next)=>{
-    const {rating,comment,productId}=req.body;
+// Create or update review
+exports.createProductReview = catchAsyncErrors(async (req, res, next) => {
+    const { rating, comment, productId } = req.body;
 
-    const review={
-        user:req.user.id,
-        name:req.user.name,
-        rating:Number(rating),
+    const review = {
+        user: req.user.id,
+        name: req.user.name,
+        rating: Number(rating),
         comment
     };
 
-    const product=await Product.findById(productId);
+    const product = await Product.findById(productId);
 
-    if(!product){
-        return next(new ErrorHandler('Product not found',404));
+    if (!product) {
+        return next(new ErrorHandler('Product not found', 404));
     }
-    const isReviewed=product.reviews.find(
-        rev=>rev.user.toString()===req.user.id.toString()
+
+    const isReviewed = product.reviews.find(
+        rev => rev.user.toString() === req.user.id.toString()
     );
 
-    if(isReviewed){
-         product.reviews.forEach(rev=>{
-            if(rev.user.toString()===req.user.id.toString()){
-                rev.rating=rating;
-                rev.comment=comment;
+    if (isReviewed) {
+        // Update existing review
+        product.reviews.forEach(rev => {
+            if (rev.user.toString() === req.user.id.toString()) {
+                rev.rating = rating;
+                rev.comment = comment;
             }
-         });
-    }else{
+        });
+    } else {
+        // Add new review
         product.reviews.push(review);
-        product.numOfReviews=product.reviews.length;
+        product.numOfReviews = product.reviews.length;
     }
 
-    //calculate average rating
-    let totalRating=0;
-    product.reviews.forEach(rev=>{
-        totalRating+=rev.rating;
+    // Calculate average rating
+    let totalRating = 0;
+    product.reviews.forEach(rev => {
+        totalRating += rev.rating;
     });
-    product.ratings=totalRatings/product.reviews.length;
+    product.ratings = totalRating / product.reviews.length; // Fixed: was totalRatings
 
-    await product.save({validateBeforeSave:false});
+    await product.save({ validateBeforeSave: false });
+
     res.status(200).json({
-        sucess:true,
-        messgage:'Review added successfully'
+        success: true, // Fixed: was sucess
+        message: 'Review added successfully' // Fixed: was messgage
     });
 });
 
+// Get all reviews of a product
+exports.getProductReviews = catchAsyncErrors(async (req, res, next) => {
+    const product = await Product.findById(req.query.id);
 
-//get all reviews of a product
-exports.getProductReviews=catchAsyncErrors(async(req,res,next)=>{
-    const product=await Product.findById(req.query.id);
-
-    if(!product){
-        return next(new ErrorHandler('Product not found',404));
+    if (!product) {
+        return next(new ErrorHandler('Product not found', 404));
     }
+
     res.status(200).json({
-        success:true,
-        reviws:product.reviews
+        success: true,
+        reviews: product.reviews // Fixed: was reviws
     });
 });
 
-//delete review
-exports.deleteReview=catchAsyncErrors(async(req,res,next)=>{
-    const product=await Product.findById(req.query.productId);
+// Delete review
+exports.deleteReview = catchAsyncErrors(async (req, res, next) => {
+    const product = await Product.findById(req.query.productId);
 
-    if(!product){
-        return next(new ErrorHandler('Product not found',404));
+    if (!product) {
+        return next(new ErrorHandler('Product not found', 404));
     }
 
-    const reviews=product.reviews.filter(
-        rev=>rev._id.toString()!==req.query.id.toString()
+    const reviews = product.reviews.filter(
+        rev => rev._id.toString() !== req.query.id.toString()
     );
 
-    //calculate new average rating
-    let totalRating=0;
-    reviews.forEach(rev=>{
-        totalRating+=rev.rating;
-    })
+    // Calculate new average rating
+    let totalRating = 0;
+    reviews.forEach(rev => {
+        totalRating += rev.rating;
+    });
 
-    const ratings=reviews.length>0 ? totalRating / reviewa.length :0;
-    const numberOfReviews=reviews.length;
+    const ratings = reviews.length > 0 ? totalRating / reviews.length : 0; // Fixed: was reviewa
+    const numOfReviews = reviews.length; // Fixed: was numberOfReviews
 
-    await Product.findByIdAndUpadate(
+    await Product.findByIdAndUpdate( // Fixed: was findByIdAndUpadate
         req.query.productId,
         {
             reviews,
@@ -180,13 +240,14 @@ exports.deleteReview=catchAsyncErrors(async(req,res,next)=>{
             numOfReviews
         },
         {
-            new:true,
-            runValidators:true,
-            UserFindAndModify:false
+            new: true,
+            runValidators: true,
+            useFindAndModify: false // Fixed: was UserFindAndModify
         }
     );
+
     res.status(200).json({
-        success:true,
-        message:'Review deleted successfully'
-    }); 
+        success: true,
+        message: 'Review deleted successfully'
+    });
 });
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ const User = require("../models/userModel");
 const ErrorHandler = require("../utils/errorHandler.js");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const sendToken = require("../utils/jwtToken.js");
+const cloudinary = require("../config/cloudinary");
 
 //login user details
 exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
@@ -40,79 +41,106 @@ exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
     email: req.body.email,
   };
 
-  //update  avatar
+  if (req.body.avatar !== "") {
+    const user = await User.findById(req.user.id);
+
+    const imageId = user.avatar.public_id;
+
+    // Delete old avatar
+    if (imageId !== "avatars/default_avatar") {
+      await cloudinary.uploader.destroy(imageId);
+    }
+
+    // Upload new avatar
+    const myCloud = await cloudinary.uploader.upload(req.body.avatar, {
+      folder: "chronous/avatars",
+      width: 150,
+      crop: "scale",
+    });
+
+    newUserData.avatar = {
+      public_id: myCloud.public_id,
+      url: myCloud.secure_url,
+    };
+  }
+
   const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
     new: true,
     runValidators: true,
+    useFindAndModify: false,
   });
 
   res.status(200).json({
-    sucess: true,
+    success: true,
     user,
   });
 });
 
+//admin:get all users
+exports.getAllUSer = catchAsyncErrors(async (req, res, next) => {
+  const users = await User.find();
 
-//admin:get all users 
-exports.getAllUSer=catchAsyncErrors(async(req,res,next)=>{
-    const users=await User.find();
-
-    res.status(200).json({
-        success:true,
-        users
-    });
-})
+  res.status(200).json({
+    success: true,
+    users,
+  });
+});
 
 //admin :get single user details
-exports.getUserDetails=catchAsyncErrors(async(req,res,next)=>{
-    const user=await User.findById(req.params.id);
+exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
 
-    if(!user){
-        return next(new ErrorHandler(`User not found with id: ${req.params.id}`,404));
-    }
+  if (!user) {
+    return next(
+      new ErrorHandler(`User not found with id: ${req.params.id}`, 404),
+    );
+  }
 
-    res.status(200).json({
-        success:true,
-        user
-    });
+  res.status(200).json({
+    success: true,
+    user,
+  });
 });
 
 //admin upadte user role
-exports.updateUserRole=catchAsyncErrors(async(req,res,next)=>{
-    const newUserData={
-        name:req.body.name,
-        email:req.body.email,
-        role:req.body.role
-    };
+exports.updateUserRole = catchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+    role: req.body.role,
+  };
 
-    const user =await   User.findByIdAndUpdate(req.params.id,newUserData,{
-        new:true,
-        runValidators:true
-    });
+  const user = await User.findByIdAndUpdate(req.params.id, newUserData, {
+    new: true,
+    runValidators: true,
+  });
 
-    if(!user){
-        return next(new ErrorHandler(`User not found with id: ${req.params.id},404`));
-    }
+  if (!user) {
+    return next(
+      new ErrorHandler(`User not found with id: ${req.params.id},404`),
+    );
+  }
 
-    res.status(200).json({
-        success:true,
-        user
-    });
+  res.status(200).json({
+    success: true,
+    user,
+  });
 });
 
-
 //admin:delete user
-exports.deleteUser=catchAsyncErrors(async(req,res,next)=>{
-    const user=await User.findById(req.params.id);
+exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
 
-    if(!user){
-        return next(new ErrorHandler(`User not found with id:${req.params.id}`,404));
-    }
+  if (!user) {
+    return next(
+      new ErrorHandler(`User not found with id:${req.params.id}`, 404),
+    );
+  }
 
-    await user.deleteOne();
+  await user.deleteOne();
 
-    res.status(200).json({
-        success:true,
-        message:'User deleted successfully'
-    })
-})
+  res.status(200).json({
+    success: true,
+    message: "User deleted successfully",
+  });
+});
